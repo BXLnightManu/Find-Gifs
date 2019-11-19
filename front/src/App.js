513a0cd1565b3de112d0d7142b8d03393a518a75
Gaston Ndanyuzwe <ndanyuzwe@hotmail.com>
2019-11-19 21:10:46 +0100

    implement localstorage use for token persistence

--- front/src/App.js
+++ front/src/App.js
@@ -1,30 +1,56 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
-import { SignIn, SignUp, PopUp, Favorites } from './containers';
-import { NavTabs } from './components';
+import React, { useEffect } from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { SignIn, SignUp, PopUp, Favorites } from "./containers";
+import { NavTabs } from "./components";
 
 // HOC function that check if user is authenticated before giving access to requested component.
-import { requireAuth, requireNoAuth } from './hoc';
+import { requireAuth, requireNoAuth } from "./hoc";
 
 // Material-UI: Calling javascript file containing the definition of styles to apply to App component.
-import { useStyles } from './styles/appStyles';
+import { useStyles } from "./styles/appStyles";
 
 function App() {
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={requireNoAuth(SignIn)} />
-          <Route exact path="/signin" component={requireNoAuth(SignIn)} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/gifsearch" component={requireAuth(NavTabs)} />
-          <Route exact path="/favorites" component={requireAuth(Favorites)} />
-        </Switch>
-    </Router>
-    <PopUp />
-    </div>
-  );
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            const action = {
+                type: "AUTH",
+                token
+            };
+            dispatch(action);
+        }
+    }, []);
+
+    return (
+        <div className={classes.root}>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={requireNoAuth(SignIn)} />
+                    <Route
+                        exact
+                        path="/signin"
+                        component={requireNoAuth(SignIn)}
+                    />
+                    <Route exact path="/signup" component={SignUp} />
+                    <Route
+                        exact
+                        path="/gifsearch"
+                        component={requireAuth(NavTabs)}
+                    />
+                    <Route
+                        exact
+                        path="/favorites"
+                        component={requireAuth(Favorites)}
+                    />
+                </Switch>
+            </Router>
+            <PopUp />
+        </div>
+    );
 }
 
 export default App;
--- front/src/containers/SignIn.jsx
+++ front/src/containers/SignIn.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useRef, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch } from  'react-redux';
-import { TextField, Button } from '@material-ui/core';
-import { useStylesForSign } from '../components/styles';
+import React, { useState, useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { TextField, Button } from "@material-ui/core";
+import { useStylesForSign } from "../components/styles";
 
-export const SignIn = (props) => {
+export const SignIn = props => {
     const dispatch = useDispatch();
     const classes = useStylesForSign();
-    const [state, setState] = useState({email:"", password:""});
+    const [state, setState] = useState({ email: "", password: "" });
     const inputRefEmail = useRef(null);
     const inputRefPwd = useRef(null);
     const submitRef = useRef(null);
@@ -20,48 +20,55 @@ export const SignIn = (props) => {
         inputEmail.addEventListener("input", handleChange);
         inputPwd.addEventListener("input", handleChange);
         submit.addEventListener("submit", handleSubmit);
-            return function clean() {
-                inputEmail.removeEventListener("input", handleChange);
-                inputPwd.removeEventListener("input", handleChange);
-                submit.removeEventListener("submit", handleSubmit);
-            }
-    })
+        return function clean() {
+            inputEmail.removeEventListener("input", handleChange);
+            inputPwd.removeEventListener("input", handleChange);
+            submit.removeEventListener("submit", handleSubmit);
+        };
+    });
 
-    const handleChange = (e) => {
+    const handleChange = e => {
         let name = e.target.name;
-        setState({...state, [name]: e.target.value});
-    }
+        setState({ ...state, [name]: e.target.value });
+    };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = e => {
         e.preventDefault();
         const CONFIG = {
-            method:     "POST",
-            headers:    new Headers({"Content-Type": "Application/json"}),
-            body:       JSON.stringify(state)
+            method: "POST",
+            headers: new Headers({ "Content-Type": "Application/json" }),
+            body: JSON.stringify(state)
         };
         const path = "/auth/signin";
         fetch(path, CONFIG)
             .then(res => res.json())
             .then(res => {
-                dispatch(
-                    {
-                        type : "AUTH",
-                        token : res.token,
-                        message : res.flash
-                    }
-                )
+                const token = res.token;
+                localStorage.setItem("token", token);
+                dispatch({
+                    type: "AUTH",
+                    token,
+                    message: res.flash
+                });
                 res.redirect && props.history.push("/gifsearch");
-                }
-            )
+            })
             .catch(err => {
                 console.error(err);
-            })
-    }
+            });
+    };
 
     return (
         <div className={classes.root}>
             <div className={classes.container}>
-                <Button className={classes.signUpButton} variant="contained" color="secondary" ><Link className={classes.linkButton} to="/signup">Sign Up</Link></Button>
+                <Button
+                    className={classes.signUpButton}
+                    variant="contained"
+                    color="secondary"
+                >
+                    <Link className={classes.linkButton} to="/signup">
+                        Sign Up
+                    </Link>
+                </Button>
                 <h1>Sign In</h1>
                 <form id="form" ref={submitRef}>
                     <TextField
@@ -84,9 +91,11 @@ export const SignIn = (props) => {
                         label="Password"
                         margin="normal"
                     />
-                    <Button variant="contained" color="primary" type="submit">Submit</Button>
+                    <Button variant="contained" color="primary" type="submit">
+                        Submit
+                    </Button>
                 </form>
             </div>
         </div>
-    )
-}
+    );
+};
